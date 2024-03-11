24f7921b96df578a870c33bfe583b2d0f5675291
Air <160431936+AirDead@users.noreply.github.com>
2024-03-11 09:45:09 +0200

    Update ClockCommand.java

--- src/main/java/me/cosmodev/Commands/ClockCommand.java
+++ src/main/java/me/cosmodev/Commands/ClockCommand.java
@@ -3,29 +3,24 @@ package me.cosmodev.Commands;
 import me.cosmodev.Plugin;
 import me.cosmodev.Utils.BuildUtil;
 import org.bukkit.Location;
-import org.bukkit.block.CommandBlock;
 import org.bukkit.command.*;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.List;
 import java.util.TimeZone;
 
 public class ClockCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command __, @NotNull String ___, @NotNull String[] args) {
-
-        // Задумка в том чтобы в usage написать что комманда только для игроков
         if (sender instanceof ConsoleCommandSender) return false;
 
         Player player = (Player) sender;
-
         Location clockLocation = player.getLocation().clone();
 
         String region = Plugin.getInstance().getConfig().getString("region");
@@ -33,22 +28,26 @@ public class ClockCommand implements CommandExecutor {
             region = "Europe/Moscow";
         }
 
-        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
-        sdf.setTimeZone(TimeZone.getTimeZone(region));
-        String currentTime = sdf.format(new Date());
+        new BukkitRunnable() {
+            public void run() {
+                SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+                sdf.setTimeZone(TimeZone.getTimeZone(region));
+                String currentTime = sdf.format(new Date());
 
-        try {
-            Date date = sdf.parse(currentTime);
-            Calendar calendar = Calendar.getInstance();
-            calendar.setTime(date);
+                try {
+                    Date date = sdf.parse(currentTime);
+                    Calendar calendar = Calendar.getInstance();
+                    calendar.setTime(date);
 
-            int hours = calendar.get(Calendar.HOUR_OF_DAY);
-            int minutes = calendar.get(Calendar.MINUTE);
+                    int hours = calendar.get(Calendar.HOUR_OF_DAY);
+                    int minutes = calendar.get(Calendar.MINUTE);
 
-            BuildUtil.displayTimeWithBlocks(clockLocation, hours, minutes);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
+                    BuildUtil.displayTimeWithBlocks(clockLocation, hours, minutes);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.runTaskTimer(Plugin.getInstance(), 0L, 1200L); 
 
         return true;
     }
